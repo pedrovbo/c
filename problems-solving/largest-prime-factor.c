0af29e5c10c61b1b5ffe3c2dd58a47b5ab0783ab
pedrovbo <pedrovicboliveira@gmail.com>
2021-10-13 00:17:19 -0300

    add a solution of a largest prime factor problem

---
+++ problems-solving/largest-prime-factor.c
@@ -0,0 +1,34 @@
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+
+#include <stdio.h>
+
+int main()
+{
+    int primeFactors = 1, largestPrimeFactor = 0;
+    long number = 600851475143;
+
+    while (number != 1)
+    {
+
+        if (number % primeFactors == 0)
+        {
+
+            number = number / primeFactors;
+
+            if (primeFactors > largestPrimeFactor)
+            {
+
+                largestPrimeFactor = primeFactors;
+            }
+        }
+        if (number != 1)
+        {
+            primeFactors++;
+        }
+    }
+
+    printf("\nThe largest prime factor of the number 600851475143 is: %d\n\n", primeFactors);
+
+    return 0;
+}
--- problems-solving/multiples-of-3-or-5.c
+++ problems-solving/multiples-of-3-or-5.c
@@ -2,17 +2,20 @@
 //  If we list all the natural numbers below 10 that are multiples of 3 or 5,
 //  we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //  Find the sum of all the multiples of 3 or 5 below 1000.
+
 #include <stdio.h>
 
 int main()
 {
     int a = 3, b = 5;
-    
-    for(int i = 0; i < 1000; i++){
-        if(i%a == 0 || i%b == 0){
-            printf("Múltiplos de 3 ou 5:  %d\n", i);
+
+    for (int i = 0; i < 1000; i++)
+    {
+        if (i % a == 0 || i % b == 0)
+        {
+            printf("Multiples of 3 or 5:  %d\n", i);
         }
     }
-    
+
     printf("\n\n");
 }

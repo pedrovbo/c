f0555cf1feb3ac53df49e5285811fea8e042e154
pedrovbo <pedrovicboliveira@gmail.com>
2021-10-26 02:13:47 -0300

    formatting code

--- estrutura-de-dados/alocacao_de_memoria.c
+++ estrutura-de-dados/alocacao_de_memoria.c
@@ -2,23 +2,23 @@
 
 static int a = 0; //  variável global, alocação estática
 
-void incrementa(void){
-    int b = 0; //  variável local, alocação automática
+void incrementa(void)
+{
+    int b = 0;        //  variável local, alocação automática
     static int c = 0; //  variável local, alocação estática
 
-    printf("a: %d, b: %d, c: %d\n", a, b,c);
+    printf("a: %d, b: %d, c: %d\n", a, b, c);
     a++;
     b++;
     c++;
 }
 
-int main(void){
+int main(void)
+{
     int i;
-    for ( i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)
     {
         incrementa();
-        printf("\n\n");        
-        return(0);
     }
-    
+    return (0);
 }

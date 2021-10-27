4698c421ed0dd637fd552aa466c1ce643a4f6483
pedrovbo <pedrovicboliveira@gmail.com>
2021-10-27 01:00:04 -0300

    add more pointer studies

---
+++ estrutura-de-dados/teste-operacao-ponteiro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int * pt;
+    /* 4 x sizeof(int) = 16 bytes*/
+    pt = pt + 4; // aqui o ponteiro estará apontando para quatro elementos adianta no endereço de memória.
+    
+
+    return 0;
+}
